4f2ff2f0413b30550fa40a542dc155059ea26b35
Max Hrytsia <maxeemja.inc@gmail.com>
2024-07-31 22:42:55 +0300

    update, delete done

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -59,7 +59,9 @@ const getPlacesByUserId = async (req, res, next) => {
 const createPlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    next(new HttpError("Invalid inputs passed, please check your data.", 422));
+    return next(
+      new HttpError("Invalid inputs passed, please check your data.", 422),
+    );
   }
 
   const { title, description, address, creator } = req.body;
@@ -91,28 +93,40 @@ const createPlace = async (req, res, next) => {
   res.status(201).json({ place: createdPlace });
 };
 
-const updatePlace = (req, res, next) => {
+const updatePlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log(errors);
-    throw new HttpError("Invalid inputs passed", 422);
+    return next(new HttpError("Invalid inputs passed", 422));
   }
 
   const { title, description } = req.body;
   const placeId = req.params.pid;
-  const updatedPlace = {
-    ...DUMMY_PLACES.find((p) => p.id === placeId),
-    title,
-    description,
-  };
+  let place;
 
-  DUMMY_PLACES = DUMMY_PLACES.map((p) => (p.id !== placeId ? p : updatedPlace));
-  res.json({ place: updatedPlace });
+  try {
+    const options = { new: true };
+    place = await Place.findByIdAndUpdate(
+      placeId,
+      { title, description },
+      options,
+    );
+  } catch (e) {
+    const error = new HttpError("Updating place failed", 500);
+    return next(error);
+  }
+
+  res.json({ place });
 };
 
-const deletePlace = (req, res, next) => {
+const deletePlace = async (req, res, next) => {
   const placeId = req.params.pid;
-  DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
+  try {
+    await Place.findByIdAndDelete(placeId);
+  } catch (e) {
+    const error = new HttpError("Deleting place failed", 500);
+    return next(error);
+  }
   res.json({ message: "Deleted place." });
 };
 
